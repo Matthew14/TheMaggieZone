26c93c351a003143eff046908854c3a596f3ae9d
Matthew O'Neill <matthew@matthewoneill.com>
2023-07-19 21:48:55 +0100

    add header title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Welcome to the Maggie.Zone</title>
+        <meta name="theme-color" content="#723d8f" />
+      </Head>
       <body>
         <Main />
         <NextScript />
