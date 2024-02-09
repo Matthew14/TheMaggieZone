b0c8c158b39447e6039ed57edf993febf926e819
Matthew O'Neill <matthew@matthewoneill.com>
2024-02-09 19:02:38 +0000

    feat: auth buttons and actual password stuff

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,29 +1,76 @@
+import { PrismaClient } from '@prisma/client';
+import { compare } from 'bcrypt';
 import type { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials'; 
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const options: NextAuthOptions = {
     providers: [
         CredentialsProvider({
             name: "Credentials",
-            credentials: { 
+            credentials: {
                 username: {
                     label: 'Username:',
                     type: 'text',
-                    placeholder: 'username goes here'
+                    placeholder: 'Type your username'
                 },
                 password: {
                     label: 'Password:',
-                    type: 'password'
+                    type: 'password',
+                    placeholder: 'PWizzle'
                 }
             },
             async authorize(credentials) {
-                const user = { id: "1", name: 'matthew', password: 'myPAss' }
-                if(credentials?.username === user.name && credentials?.password === user.password) {
-                    return user
+                const prisma = new PrismaClient()
+
+                if (!credentials?.password || !credentials.username) {
+                    return null;
                 }
-                else { return null }
+                const user = await prisma.applicationUser.findUnique({
+                    where: { username: credentials.username }
+                })
+
+                if (!user) {
+                    return null;
+                }
+                const isPasswordValid = await compare(
+                    credentials.password,
+                    user.password
+                )
+                if (!isPasswordValid) {
+                    return null
+                }
+
+                return {
+                    id: user.id + '',
+                    email: user.email,
+                    name: user.username
+                }
             }
-            
+
         })
     ],
+
+    callbacks: {
+        session: ({ session, token }) => {
+            console.log('Session Callback', { session, token })
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id,
+                }
+            }
+        },
+        jwt: ({ token, user }) => {
+            console.log('JWT Callback', { token, user })
+            if (user) {
+                const u = user as unknown as any
+                return {
+                    ...token,
+                    id: u.id
+                }
+            }
+            return token
+        }
+    }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import { Inter } from "next/font/google";
 
 import MaggieImageList from "@/components/MaggieImageList";
 import { S3Client, paginateListObjectsV2 } from "@aws-sdk/client-s3";
+import { LoginButton, LogoutButton } from "./components/Buttons";
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]/options";
 
 const inter = Inter({ subsets: ["latin"] });
-const bucket = 'the-maggie-zone-images' 
+const bucket = 'the-maggie-zone-images'
 const Page: React.FC = async () => {
 
     const s3Client = new S3Client({ region: 'eu-west-1', });
@@ -12,30 +15,34 @@ const Page: React.FC = async () => {
     const paginator = paginateListObjectsV2(
         { client: s3Client },
         { Bucket: bucket }
-      );
-      const getImages = async () => {
-      for await (const page of paginator) {
-        const objects = page.Contents;
-        if (objects) {
-          return objects.map((o) => { return {title: o.Key!, img: `${s3Url}/${o.Key}`}})
+    );
+    const getImages = async () => {
+        for await (const page of paginator) {
+            const objects = page.Contents;
+            if (objects) {
+                return objects.map((o) => { return { title: o.Key!, img: `${s3Url}/${o.Key}` } })
+            }
         }
-    }   
     }
 
     const imagesData = await getImages();
+    const session = await getServerSession(options);
     return imagesData && (
 
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
-        <div>
-            <div className='flex mx-auto'>
-            <div className='flex-col text-center'>
-                <h1 className='text-5xl text-white justify-center text-center'>
-                Welcome to the Maggie Zone
-                </h1>
+            <div>
+                <div className="authButton">
+                    {session ? <LogoutButton /> : <LoginButton /> }
+                </div>
+                <div className='flex mx-auto'>
+                    <div className='flex-col text-center'>
+                        <h1 className='text-5xl text-white justify-center text-center'>
+                            Welcome to the Maggie Zone
+                        </h1>
+                    </div>
+                </div>
+                <MaggieImageList images={imagesData} />
             </div>
-            </div>
-            <MaggieImageList images={imagesData} />
-        </div>
         </main>
     );
 };
